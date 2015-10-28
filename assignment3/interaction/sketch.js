3b0ed8baba25097f375f847291fc6727c02c5187
sidds1009 <Jerry@admins-MacBook-Pro.local>
2015-10-28 18:45:13 -0400

    Homework

---
+++ assignment3/interaction/sketch.js
@@ -0,0 +1,40 @@
+function setup() {
+  createCanvas(400,600);
+}
+
+function draw() {
+  background(229, 228, 215)
+  noStroke();
+  fill(66, 28, 82);
+  quad(180,10,290,35,210,180,mouseX,mouseY);
+  
+  noStroke();
+  fill(255, 204, 0);
+  quad(163, 320, 239, 328, 214, 435, 168, 435);
+  
+  noStroke();
+  fill(0, 51, 102);
+  quad(60, 392, 295, 424, 295, 435, 60, 403);
+  
+  noStroke();
+  fill(0,0,0);
+  quad(85, 376, 145, 376, 145, 442, 85, 442);
+  
+  noStroke();
+  fill(0,0,0);
+  quad(118,476,170,450,185,474,mouseX,mouseY);
+  
+  noStroke();
+  fill(255,0,0);
+  quad(163, 490, 205, 480, 205, 499, 163, 510);
+  
+  noStroke();
+  fill(255,0,0);
+  quad(190, 510, 248, 507, 247, 513, 190, 515);
+
+  noStroke();
+  fill(44, 103, 0);
+  ellipse(215,542,mouseX,mouseY);
+
+  
+}
